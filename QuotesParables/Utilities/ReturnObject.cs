eb417bf591d4cc783c8bd27c5036fcffcb513647
stanmerrill <stanmbus@yahoo.com>
2023-03-03 17:03:52 -0600

    updates

--- QuotesParables/Utilities/ReturnObject.cs
+++ QuotesParables/Utilities/ReturnObject.cs
@@ -8,6 +8,7 @@ namespace QuotesParables.Utilities
     public class ReturnObject
     {
         public List<string> ErrorMessages = new List<string>();
+        public string returnString = "";
         public Object ReturnObj1 = null;
         public Object ReturnObj2 = null;
         public Object ReturnObj3 = null;
